e06f0d9c5c6686e4a7d02c8860a12e90ee67a66b
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-13 03:23:14 -0800

    initial commit

---
+++ client/src/components/content/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import {} from "../../../../"
+
+const Content = () => {
+    return (
+        <div>
+            <video>
+                <source src="../../../../1.mp4" type="video/mp4" />
+                <source src="" type="" />
+                <source src="" type="" />
+                Something went wrong!
+            </video>
+        </div>
+    )
+}
+
+export default Content
