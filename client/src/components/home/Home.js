1dfeb97f198531b5326acf8a09d91a8599dc2076
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-14 15:57:34 +0530

    setup basic component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Content from '../content/Content'
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Content from '../content/Content';
 
 const Home = () => {
-    return (
-        <div>
-          <Content />  
-        </div>
-    )
-}
+	return (
+		<Grid container justify='center' xs={12} md={9} lg={8}>
+			{/* <Grid item> */}
+			{/* <Grid container item> */}
+			<Content />
+			{/* </Grid> */}
+			{/* </Grid> */}
+		</Grid>
+	);
+};
 
-export default Home
+export default Home;
