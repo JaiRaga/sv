71f76dbba1fc0a2a755f49574cbd3c9282404b0c
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-14 15:58:54 +0530

    added home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import Home from "./components/home/Home"
+import Home from './components/home/Home';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+	return (
+		<div>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
