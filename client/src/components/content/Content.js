2c8913cdea54572a2b566f822a4c285f2e042630
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-14 15:58:12 +0530

    created basic video layout

--- client/src/components/content/Content.js
+++ client/src/components/content/Content.js
@@ -1,17 +1,51 @@
-import React from 'react'
-// import {} from "../../../../"
+import React from 'react';
+import { Grid, makeStyles } from '@material-ui/core';
+
+import one from '../../sample/1.mp4';
+import two from '../../sample/2.mp4';
+import three from '../../sample/3.mp4';
+import samplePic from '../../sample.png';
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		background: '#888',
+	},
+}));
 
 const Content = () => {
-    return (
-        <div>
-            <video>
-                <source src="../../../../1.mp4" type="video/mp4" />
-                <source src="" type="" />
-                <source src="" type="" />
-                Something went wrong!
-            </video>
-        </div>
-    )
-}
+	const classes = useStyles();
+	return (
+		<Grid
+			container
+			item
+			xs={12}
+			md={9}
+			lg={8}
+			justify='center'
+			className={classes.container}>
+			<Grid item>
+				<video
+					width='300px'
+					height='300px'
+					controls
+					autoplay
+					muted
+					poster={samplePic}>
+					<source src={one} />
+				</video>
+			</Grid>
+			<Grid item>
+				<video width='300px' height='300px' controls>
+					<source src={two} />
+				</video>
+			</Grid>
+			<Grid item>
+				<video width='300px' height='300px' autoplay>
+					<source src={three} />
+				</video>
+			</Grid>
+		</Grid>
+	);
+};
 
-export default Content
+export default Content;
