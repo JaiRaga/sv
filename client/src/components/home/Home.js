7edb142e80e7d83a2082fcd9a21273d62f4cb262
JaiRaga <raga.j.santhosh@gmail.com>
2020-12-21 08:39:55 +0530

    testing camera component

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Content from '../content/Content';
+import CameraPage from '../camera/CameraPage';
 
 const Home = () => {
 	return (
-		<Grid container justify='center' xs={12} md={9} lg={8}>
+		<Grid container justify='center'>
 			{/* <Grid item> */}
 			{/* <Grid container item> */}
-			<Content />
+			{/* <Content /> */}
+			<CameraPage />
 			{/* </Grid> */}
 			{/* </Grid> */}
 		</Grid>
